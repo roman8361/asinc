274fed619cf2dd207b4e48f76437ef5c429f78bc
Кравченко Роман Владимирович (4061571) <rvkravchenko@vtb.ru>
2024-04-03 11:33:29 +0300

    first commit

---
+++ common/src/main/java/ru/kravchenko/common/CommonApp.java
@@ -0,0 +1,13 @@
+package ru.kravchenko.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonApp.class, args);
+    }
+
+}
